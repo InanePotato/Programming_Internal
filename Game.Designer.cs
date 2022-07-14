5ff24da7409580c93ace728b7f5f3f4566445681
Michael Burke <mdburke379@gmail.com>
2022-07-14 14:24:53 +1200

    PNL_Game stays center & added form  background image

--- Game.Designer.cs
+++ Game.Designer.cs
@@ -29,24 +29,41 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
+            this.PNL_Game = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // PNL_Game
+            // 
+            this.PNL_Game.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.PNL_Game.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.PNL_Game.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.PNL_Game.Location = new System.Drawing.Point(0, 0);
+            this.PNL_Game.Name = "PNL_Game";
+            this.PNL_Game.Size = new System.Drawing.Size(886, 762);
+            this.PNL_Game.TabIndex = 0;
             // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(884, 761);
+            this.Controls.Add(this.PNL_Game);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(900, 800);
             this.Name = "Game";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "The Duck Song - But It\'s A Game";
             this.Load += new System.EventHandler(this.Game_Load);
+            this.SizeChanged += new System.EventHandler(this.Game_SizeChanged);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel PNL_Game;
     }
 }
 
--- Game.cs
+++ Game.cs
@@ -12,6 +12,7 @@ namespace Programming_Internal
 {
     public partial class Game : Form
     {
+        Image IMG_FormBackground = Image.FromFile(Application.StartupPath + @"\images\Game_Form_Background_Image.png");
         public Game()
         {
             InitializeComponent();
@@ -19,7 +20,21 @@ namespace Programming_Internal
 
         private void Game_Load(object sender, EventArgs e)
         {
+            //this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
+        }
 
+        private void Game_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.BackgroundImage = null;
+                PNL_Game.Location = new Point(0,0);
+            }
+            else
+            {
+                this.BackgroundImage = IMG_FormBackground;
+                PNL_Game.Location = new Point((Screen.FromHandle(this.Handle).WorkingArea.Width - PNL_Game.Width)/2, (Screen.FromHandle(this.Handle).WorkingArea.Height - PNL_Game.Height)/2);
+            }
         }
     }
 }
