19dd7861c8cd796efbe46064089a653e71f4a2f3
Michael Burke <mdburke379@gmail.com>
2022-10-05 12:40:42 +1300

    Got rid of a un-used class, and comented on some code.

--- Admin.cs
+++ Admin.cs
@@ -13,8 +13,15 @@ namespace Programming_Internal
 {
     public partial class Admin : Form
     {
-        bool showingSnap = false;
+        //---------------------------------------------//
+        // Declares public variables used in this form //
+        //---------------------------------------------//
+        bool showingSnap = false; // used to tell if the form is suposed to be 'snapped' and if it accualy is or not
 
+        //--------------------------------------------------------------//
+        // Declares variables used for rounding the corners of the form //
+        //--------------------------------------------------------------//
+        
         [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
         private static extern IntPtr CreateRoundRectRgn
         (
@@ -29,23 +36,28 @@ namespace Programming_Internal
         public Admin()
         {
             InitializeComponent();
+            // sets the form border style property of the form to none (no border)
             this.FormBorderStyle = FormBorderStyle.None;
+            // Rounds corners of the form using code from above
             Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
         }
 
         private void Admin_Load(object sender, EventArgs e)
         {
+            // sets values for the global variables regarding this form
             GlobalVariables.AdminSnap = true;
             GlobalVariables.CloseAdmin = false;
         }
 
         private void BTN_Home_Click(object sender, EventArgs e)
         {
-            if (BTN_Home.Cursor == Cursors.Hand)
+            if (BTN_Home.Cursor == Cursors.Hand) // checks if the admin home form isn't already showing
             {
+                // change background color and hover cursor of the home button
                 BTN_Home.BackColor = Color.FromArgb(46, 51, 73);
                 BTN_Home.Cursor = Cursors.Default;
 
+                // makes sure all the other buttons are able to be clicked & are with default properties
                 BTN_LiveVariables.BackColor = Color.FromArgb(24, 30, 54);
                 BTN_UnitSettings.BackColor = Color.FromArgb(24, 30, 54);
                 BTN_Users.BackColor = Color.FromArgb(24, 30, 54);
@@ -53,20 +65,26 @@ namespace Programming_Internal
                 BTN_UnitSettings.Cursor = Cursors.Hand;
                 BTN_Users.Cursor = Cursors.Hand;
 
+                // calls on the child form openChildAdminForm event with a new instance of the Admin_Home form
                 openChildAdminForm(new Admin_Home(GlobalVariables.AdminSnap));
+                // sets the value for the global variabe used to store what admin page is currently open to 'home'
                 GlobalVariables.SnappedAdminWindowOpen = "home";
 
+                // sets the title at the top of the page to display "Home" as it's text
+                // note: this will only show if the admin forms are not 'snapped'
                 lbl_Title.Text = "Home";
             }
         }
 
         private void BTN_LiveVariables_Click(object sender, EventArgs e)
         {
-            if (BTN_LiveVariables.Cursor == Cursors.Hand)
+            if (BTN_LiveVariables.Cursor == Cursors.Hand) // checks if the admin live variables form isn't already showing
             {
+                // change background color and hover cursor of the live variables button
                 BTN_LiveVariables.BackColor = Color.FromArgb(46, 51, 73);
                 BTN_LiveVariables.Cursor = Cursors.Default;
 
+                // makes sure all the other buttons are able to be clicked & are with default properties
                 BTN_Home.BackColor = Color.FromArgb(24, 30, 54);
                 BTN_UnitSettings.BackColor = Color.FromArgb(24, 30, 54);
                 BTN_Users.BackColor = Color.FromArgb(24, 30, 54);
@@ -74,41 +92,52 @@ namespace Programming_Internal
                 BTN_UnitSettings.Cursor = Cursors.Hand;
                 BTN_Users.Cursor = Cursors.Hand;
 
+                // calls on the child form openChildAdminForm event with a new instance of the Admin_liveVariables form
                 openChildAdminForm(new Admin_LiveVariables(GlobalVariables.AdminSnap));
+                // sets the value for the global variabe used to store what admin page is currently open to 'live_vars'
                 GlobalVariables.SnappedAdminWindowOpen = "live_vars";
 
+                // sets the title at the top of the page to display "Live variables" as it's text
+                // note: this will only show if the admin forms are not 'snapped'
                 lbl_Title.Text = "Live Variables";
             }
         }
 
         private void BTN_UnitSettings_Click(object sender, EventArgs e)
         {
-            if (BTN_UnitSettings.Cursor == Cursors.Hand)
+            if (BTN_UnitSettings.Cursor == Cursors.Hand) // checks if the admin unit settings form isn't already showing
             {
+                // change background color and hover cursor of the unit settings button
                 BTN_UnitSettings.BackColor = Color.FromArgb(46, 51, 73);
                 BTN_UnitSettings.Cursor = Cursors.Default;
 
+                // makes sure all the other buttons are able to be clicked & are with default properties
                 BTN_Home.BackColor = Color.FromArgb(24, 30, 54);
                 BTN_LiveVariables.BackColor = Color.FromArgb(24, 30, 54);
                 BTN_Users.BackColor = Color.FromArgb(24, 30, 54);
                 BTN_Home.Cursor = Cursors.Hand;
                 BTN_LiveVariables.Cursor = Cursors.Hand;
                 BTN_Users.Cursor = Cursors.Hand;
-
+                // calls on the child form openChildAdminForm event with a new instance of the Admin_UnitSettings form
                 openChildAdminForm(new Admin_UnitSettings(GlobalVariables.AdminSnap));
+                // sets the value for the global variabe used to store what admin page is currently open to 'unit_settings'
                 GlobalVariables.SnappedAdminWindowOpen = "unit_settings";
 
+                // sets the title at the top of the page to display "Unit Settings" as it's text
+                // note: this will only show if the admin forms are not 'snapped'
                 lbl_Title.Text = "Unit Settings";
             }
         }
 
         private void BTN_Users_Click(object sender, EventArgs e)
         {
-            if (BTN_Users.Cursor == Cursors.Hand)
+            if (BTN_Users.Cursor == Cursors.Hand) // checks if the admin users / game saves form isn't already showing
             {
+                // change background color and hover cursor of the users / game saves button
                 BTN_Users.BackColor = Color.FromArgb(46, 51, 73);
                 BTN_Users.Cursor = Cursors.Default;
 
+                // makes sure all the other buttons are able to be clicked & are with default properties
                 BTN_Home.BackColor = Color.FromArgb(24, 30, 54);
                 BTN_LiveVariables.BackColor = Color.FromArgb(24, 30, 54);
                 BTN_UnitSettings.BackColor = Color.FromArgb(24, 30, 54);
@@ -116,111 +145,173 @@ namespace Programming_Internal
                 BTN_LiveVariables.Cursor = Cursors.Hand;
                 BTN_UnitSettings.Cursor = Cursors.Hand;
 
+                // calls on the child form openChildAdminForm event with a new instance of the Admin_Saves form
                 openChildAdminForm(new Admin_Saves(GlobalVariables.AdminSnap));
+                // sets the value for the global variabe used to store what admin page is currently open to 'saves'
                 GlobalVariables.SnappedAdminWindowOpen = "saves";
 
-                lbl_Title.Text = "Saves";
+                // sets the title at the top of the page to display "Game Saves" as it's text
+                // note: this will only show if the admin forms are not 'snapped'
+                lbl_Title.Text = "Game Saves";
             }
         }
 
         private void BTN_Exit_Click(object sender, EventArgs e)
         {
+            // sets all global bariables invloved in the closing of the admin forms to what they should be
+            // this ensures everything that needs to close does so, and helps restrict any issues on the re-opening
             GlobalVariables.CloseAdmin = true;
             GlobalVariables.AdminMode = false;
             GlobalVariables.AdminSnap = false;
 
+            // closes the Admin form
             this.Close();
         }
 
+        // declares the private variable used for storing the current admin chuld form (sub-form) that is open
         private Form activeAdminForm = null;
-        private void openChildAdminForm(Form childAdminForm)
+
+
+        //-----------------------------------------------------------------//
+        // The event called / used for opening new child forms (sub-forms) //
+        //-----------------------------------------------------------------//
+        private void openChildAdminForm(Form childAdminForm) // when this event is called (requires a instance of a form to be inputted when called)
         {
-            if (activeAdminForm != null)
+            if (activeAdminForm != null) // checks if there is currently a child form open
             {
+                // if another child form is currently open, this closes it before continuing on with opening the new one
                 activeAdminForm.Close();
             }
 
-            if (GlobalVariables.AdminSnap == true)
+            if (GlobalVariables.AdminSnap == true) // checks if the child form is supposed to be 'snapped' to the side of the screen
             {
+                // opens the child form normaly, without any extra change of properties
                 childAdminForm.Show();
             }
             else
             {
-                activeAdminForm = childAdminForm;
-                childAdminForm.TopLevel = false;
-                childAdminForm.FormBorderStyle = FormBorderStyle.None;
-                childAdminForm.Dock = DockStyle.Fill;
-                PNL_Body.Controls.Add(childAdminForm);
-                PNL_Body.Tag = childAdminForm;
-                childAdminForm.BringToFront();
-                childAdminForm.Show();
+                activeAdminForm = childAdminForm; // sets the active child form to the form about to be opened
+                childAdminForm.TopLevel = false; // makes sure the child form is correctly placed
+                childAdminForm.FormBorderStyle = FormBorderStyle.None; // makes sure the child form has no border
+                childAdminForm.Dock = DockStyle.Fill; // makes sure the chuld form is resized to fully fit the panel
+                PNL_Body.Controls.Add(childAdminForm); // places the child form into the panel
+                PNL_Body.Tag = childAdminForm; // gives the panel the tag of the child form
+                childAdminForm.BringToFront(); // makes sure the child form is at the frount of everything else in the panel
+                childAdminForm.Show(); // shows / opens the child form
             }
         }
 
-        private void closeChildForm()
+        //-----------------------------------------------------------------//
+        // The event called / used for closing new child forms (sub-forms) //
+        //-----------------------------------------------------------------//
+        private void closeChildForm() // when this event is called
         {
-            if (activeAdminForm != null)
+            if (activeAdminForm != null) // checks if there is currently a child form open
             {
+                // if there is currently a child form open, closes the current open child form
                 activeAdminForm.Close();
             }
         }
 
         private void TMR_Checker_Tick(object sender, EventArgs e)
         {
+            //----------------------------------------------------------//
+            // Handles the resizing and change of layouts of the child  //
+            // forms depending on whether they should be snapped or not //
+            //----------------------------------------------------------//
+
+            // checks if the global variable for whether the admin forms should be snapped to the side is true (the forms should be snapped)
             if (GlobalVariables.AdminSnap == true)
             {
+                // checks if the admin forms are currently snapped to the side
                 if (showingSnap == false)
                 {
+                    // if they aren't currently snapped to the side but because they are supposed to (according to the privious if statment) start showing them as snapped
                     showingSnap = true;
 
+                    // closes the current 'unsnapped' child form
                     closeChildForm();
                 }
             }
+            // if the first statment is false, then check if the forms are currently snapped
+            // and if according to the appropriate global variable, they arn't supposed to be snapped
             else if (GlobalVariables.AdminSnap == false && showingSnap == true)
             {
+                // stop showing them as snapped
                 showingSnap = false;
 
+                // get rid of the current snapped child form
                 GlobalVariables.SnappedAdminWindowOpen = null;
 
+                // finds out what child form is supposed to be showing, and re-opens the correct child form un-snapped
                 if (BTN_LiveVariables.Cursor == Cursors.Default) { openChildAdminForm(new Admin_LiveVariables(GlobalVariables.AdminSnap)); }
-                else if (BTN_UnitSettings.Cursor == Cursors.Default) { }
-                else if (BTN_Users.Cursor == Cursors.Default) { }
-                else // home window should be opened
+                else if (BTN_UnitSettings.Cursor == Cursors.Default) { openChildAdminForm(new Admin_UnitSettings(GlobalVariables.AdminSnap)); }
+                else if (BTN_Users.Cursor == Cursors.Default) { openChildAdminForm(new Admin_Saves(GlobalVariables.AdminSnap)); }
+                else // home window should be opened (only other option, one must always be open at any given time)
                 {
                     openChildAdminForm(new Admin_Home(GlobalVariables.AdminSnap));
                 }
             }
 
-            if (GlobalVariables.AdminSnap == true)
+
+            //---------------------------------------------------------//
+            //  Resizes and changes the proterties of the admin form   //
+            // depending on wether it is supposed to be snapped or not //
+            //---------------------------------------------------------//
+
+            if (GlobalVariables.AdminSnap == true) // uses the appropriate global variable to check if the admin forms should be snapped
             {
+                // if they are supposed to be snapped then...
+
+                // declares the variable used to store the appropriate size for things when the forms are snapped
                 Size snappedsize = new Size(186, Screen.FromHandle(this.Handle).WorkingArea.Height);
 
+                // checks if the forms size is not currently the size it should be when snapped
                 if (this.Size != snappedsize)
                 {
-                    this.Size = new Size(186, Screen.FromHandle(this.Handle).WorkingArea.Height);
+                    // sets the current size of the form to the pre-determined, correct, snapped size
+                    this.Size = snappedsize;
+                    // places the main admin form in the top left corner of the users screen
                     this.Location = new Point(0, 0);
 
+                    // makes the corners of the form no longer curved / rounded
                     Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 0, 0));
 
+                    // makes sure the admin form is the furtherest top form out of everything on the users screen
                     this.TopMost = true;
                 }
             }
+            // otherwise if the admin forms arn't ment to be snapped
             else if (GlobalVariables.AdminSnap == false)
             {
+                // declare and set a value to the variable that stores the correct normal size for the admin form
                 Size normsize = new Size(935, 538);
 
+                // checks if the size of the admin form is the correct size that it is supposed to be
                 if (this.Size != normsize)
                 { 
-                    this.Size = new Size(935, 538);
+                    // if it isn't then it makes it the correct size
+                    this.Size = normsize;
 
+                    // rounds the corners of the form
                     Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
 
+                    // makes the form no longer the top most form on the users screen
                     this.TopMost = false;
                 }
             }
 
+
+            //--------------------------------------------------//
+            //   if their is currently no child form, set the   //
+            // buttons back to their default proterties / style //
+            //--------------------------------------------------//
+
+            // checks if there is currently no active child form
             if (activeAdminForm == null)
             {
+                // if their is no currently active / admin window open child form then...
+                // sets all the buttons style back to what the defaults are
                 BTN_Home.BackColor = Color.FromArgb(24, 30, 54);
                 BTN_LiveVariables.BackColor = Color.FromArgb(24, 30, 54);
                 BTN_UnitSettings.BackColor = Color.FromArgb(24, 30, 54);
--- Admin_Home.cs
+++ Admin_Home.cs
@@ -12,52 +12,84 @@ namespace Programming_Internal
 {
     public partial class Admin_Home : Form
     {
-        bool WindowSnapped;
+        //--------------------------------------------------------//
+        // declares all public variables used throughout the form //
+        //--------------------------------------------------------//
 
+        bool WindowSnapped; // declares the bool used to store whether the form should be snapped or not
+
+        // when the form is called to open, it requires to know if it should be snapped or not
         public Admin_Home(bool SnappedWindow)
         {
             InitializeComponent();
+
+            // sets the priviously declared bool to the vaule that was passed during the creation of the form
             WindowSnapped = SnappedWindow;
         }
 
         private void Admin_Home_Load(object sender, EventArgs e)
         {
+            // checks if the form should be snapped or not
             if (WindowSnapped == true)
             {
+                // if the form does need to be snapped, change the layout accordingly
+                // make the form the top most form on the users screen
                 this.TopMost = true;
 
+                // set the size of the form to take up the entire height of the screen and the appropriately set width
                 this.Size = new Size(278, Screen.FromHandle(this.Handle).WorkingArea.Height);
+                // set the location of the form to be in the top right corner of the users screen
                 this.Location = new Point(Screen.FromHandle(this.Handle).WorkingArea.Width - this.Width, 0);
 
+                // moves the settings panel into the top left corner of the form
                 PNL_Settings.Location = new Point(0,0);
+                // changes the settings panel size to take up the whole form
                 PNL_Settings.Size = new Size(PNL_Settings.Width, this.Height);
 
+                // dissable the visibility of the duck image
                 PIC_Duck.Visible = false;
             }
+            // if the form dosn't need to be snapped it does nothing (it's already layed out in this form)
 
+            // change the value of the snap toggle button to whatever the appropriate global variable says it should be
             toggle_Snap.Checked = GlobalVariables.AdminSnap;
         }
 
         private void TMR_Checker_Tick(object sender, EventArgs e)
         {
+            //------------------------//
+            // Constantly checking... //
+            //------------------------//
+
+            // checks if the child form should be snapped, and if the current child form open shouldn't be the home form
             if (GlobalVariables.AdminSnap == true && GlobalVariables.SnappedAdminWindowOpen != "home")
             {
+                // if the child forms are snapped, and the wrong child form is open (this form shouldn't be open)
+                // closes this form
                 this.Close();
             }
 
+            // checks if this form is snapped to the side of the screen, but it isn't supposed to
             if (WindowSnapped == true && GlobalVariables.AdminSnap == false)
             {
+                // if this form shouldn't be snapped and it is
+                // closes this form
                 this.Close();
             }
 
+            // checks if the admin forms are supposed to be closed
             if (GlobalVariables.CloseAdmin == true)
             {
+                // if this form (and all other admin forms are supposed to be closed)
+                // closes this form
                 this.Close();
             }
         }
 
         private void BTN_Submit_Click(object sender, EventArgs e)
         {
+            // whenever the button is clicked to submit the changes to everything on this form
+            // update the global variable for snapped forms to be the same as the selected option by the user
             GlobalVariables.AdminSnap = toggle_Snap.Checked;
         }
     }
